2cd3213c4ae7f56f574dfc7118e4d0da2dfd3bc1
Neerudu, Pranathi <pneerudu@iu.edu>
2023-05-26 14:33:24 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,19 @@
+import React from "react";
+import About from "./components/about";
+//import Contact from "./components/contact";
+import Navbar from "./components/navbar";
+import Projects from "./components/projects";
+import Skills from "./components/skills";
+
+export default function App() {
+
+  return(
+      <main className="text-gray-400 bg-gray-900 body-font">
+      <Navbar />
+      <About />
+      <Projects />
+      <Skills />
+      {/* <Contact /> */}
+    </main>
+    );
+}
