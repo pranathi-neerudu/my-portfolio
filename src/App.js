383b9788777984f82e5a70863875068e61220a22
Neerudu, Pranathi <pneerudu@iu.edu>
2023-05-29 16:34:24 +0530

    about test

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from "react";
-import About from "./components/About";
+import About1 from "./components/About1";
 import Navbar from "./components/navbar";
 import Projects from "./components/Projects";
 import Skills from "./components/Skills";
@@ -12,7 +12,7 @@ export default function App() {
   return(
       <main className="text-gray-400 bg-gray-900 body-font">
       <Navbar />
-      <About />
+      <About1 />
       <Projects />
       <Skills />
       <Experience />
